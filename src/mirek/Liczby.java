dbede13f32834387f0b6f9431a827e656f39d8c3
mireknosek <mireknosek@gmail.com>
2020-02-29 14:08:15 +0100

    print 3

--- src/mirek/Liczby.java
+++ src/mirek/Liczby.java
@@ -4,13 +4,9 @@ public class Liczby {
     int a;
     int b;
 
-    public void Liczby(int l1, int l2){
+    public Liczby(int l1, int l2){
         a = l1;
         b = l2;
-
-        // wyswietl
-        System.out.println("a="+a);
-        System.out.println("b"+b);
 
     }
 }
--- src/mirek/Operacje.java
+++ src/mirek/Operacje.java
@@ -1,9 +1,10 @@
 package mirek;
 
 public class Operacje {
-    int a;
-    int b;
-    public void dodaj(){
 
+    public static void main(String[] args) {
+        Liczby l1 = new Liczby(4,5);
+        System.out.println(l1.a);
     }
+
 }
