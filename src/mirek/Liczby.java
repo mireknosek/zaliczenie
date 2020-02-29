6fd00c7ef74ce8f36c96aee4836e03421edd6514
mireknosek <mireknosek@gmail.com>
2020-02-29 12:46:59 +0100

    3

--- src/mirek/Liczby.java
+++ src/mirek/Liczby.java
@@ -1,6 +1,14 @@
 package mirek;
 
+import org.w3c.dom.ls.LSOutput;
+
 public class Liczby {
     int a;
     int b;
+
+    public void Liczby(int l1, int l2){
+        a = l1;
+        b = l2;
+
+    }
 }
