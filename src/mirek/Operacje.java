60eb196c6d97f57b08f310887e0cf25f74c678c3
mireknosek <mireknosek@gmail.com>
2020-02-29 14:35:10 +0100

    zali

--- src/mirek/Operacje.java
+++ src/mirek/Operacje.java
@@ -5,7 +5,8 @@ public class Operacje {
     public static void main(String[] args) {
         Liczby l1 = new Liczby(4,5);
         System.out.println(l1.a);
-       
+        System.out.println(l1.b);
+
     }
 
 }
